2b1a6dfbf399db8cf39794c20e4bb0671132940f
Miroslav Kovac <assembler_bot@yahoo.com>
2023-10-09 12:10:25 +0200

    [Update] Added test project.

---
+++ Tests/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OdinSerializer;
+
+[Serializable]
+public class Test1
+{
+	public int IntValue;
+
+	public void Init()
+	{
+		IntValue = 3;
+	}
+}
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Serialize");
+		
+		Test1 test1_in = new();
+		test1_in.Init();
+		byte[] json = SerializationUtility.SerializeValue(test1_in, DataFormat.JSON);
+		Console.WriteLine(System.Text.Encoding.UTF8.GetString(json));
+		
+		Console.WriteLine("Deserialize");
+		
+		Test1 test1_out = SerializationUtility.DeserializeValue<Test1>(json, DataFormat.JSON);
+		
+		Console.WriteLine("Done");
+	}
+}
+
